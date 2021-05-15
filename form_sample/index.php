0a459776c47ad766044c0775e37726da1febf53f
cordelia <80014084+cordelia-sixth@users.noreply.github.com>
2021-05-15 23:59:54 +0900

    Create index.php

---
+++ form_sample/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Form sample</title>
+</head>
+<body>
+  <form action="contact.php" method="post">
+    <div>名前</div>
+    <input type="text" name="name">
+
+    <div>年齢</div>
+    <select>
+      <option value="10代">10代</option>
+      <option value="20代">20代</option>
+      <option value="30代">30代</option>
+      <option value="40代">40代</option>
+      <option value="50代">50代</option>
+      <option value="60代">60代</option>
+      <option value="70代">70代</option>
+      <option value="80代">80代</option>
+      <option value="90代">90代</option>
+    </select>
+
+    <div>内容</div>
+    <textarea></textarea>
+  </form>
+</body>
+</html>
