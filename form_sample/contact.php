632464356544a1e72c394378a34ee74f6cce9589
cordelia <80014084+cordelia-sixth@users.noreply.github.com>
2021-05-16 13:55:40 +0900

    Update contact.php

--- form_sample/contact.php
+++ form_sample/contact.php
@@ -1,1 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Contact form</title>
+</head>
+<body>
+  <div>Name:</div>
+  <?php echo $_POST['name'] ?>
+
+  <div>Age:</div>
+  <?php echo $_POST['age'] ?>
+
+  <div>Message:</div>
+  <?php echo $_POST['message'] ?>
+</body>
+</html>
