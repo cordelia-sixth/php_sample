377e832476a50968b981039a4d17f903f13e04a0
cordelia <80014084+cordelia-sixth@users.noreply.github.com>
2021-05-16 13:55:17 +0900

    Update index.php

--- form_sample/index.php
+++ form_sample/index.php
@@ -1,29 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="UTF-8">
-  <title>Form sample</title>
+  <meta charset="utf-8">
+  <title>contact form</title>
 </head>
 <body>
-  <form action="contact.php" method="post">
-    <div>名前</div>
+  <form method="post" action="contact.php">
+    <div>Name:</div>
     <input type="text" name="name">
+    
 
-    <div>年齢</div>
-    <select>
-      <option value="10代">10代</option>
-      <option value="20代">20代</option>
-      <option value="30代">30代</option>
-      <option value="40代">40代</option>
-      <option value="50代">50代</option>
-      <option value="60代">60代</option>
-      <option value="70代">70代</option>
-      <option value="80代">80代</option>
-      <option value="90代">90代</option>
+    <div>Age:</div>
+    <select name="age">
+    <option value="20代">20代</option>
     </select>
 
-    <div>内容</div>
-    <textarea></textarea>
+    <div>Message:</div>
+    <textarea name="message"></textarea>
+
+    <div>
+      <input type="submit" value="send">
+    </div>
   </form>
-</body>
+</dody>
 </html>
